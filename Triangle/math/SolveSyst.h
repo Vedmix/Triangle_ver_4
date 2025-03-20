0fa18d059f52bf15beece4c464d8dacb376ac18f
Daunito228 <rep.gep1945@gmail.com>
2025-03-20 23:20:15 +0300

    Solve system with Kramer method

--- Triangle/math/SolveSyst.h
+++ Triangle/math/SolveSyst.h
@@ -1,1 +1,11 @@
 #pragma once
+#include <iostream>
+#include <fstream>
+#include "matrix.h"
+#include <cmath>
+using namespace std;
+
+
+int** fileInput(const string& filename, int* n, int* m);
+
+int SolveKramerMethod(int** syst, int n);
